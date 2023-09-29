9d67e1c0ecc733f156e365907a7d3b9e7ef7fcc0
joaovvictor <joao.angelo@aluno.senai.br>
2023-09-29 16:18:44 -0300

    commit inicial

---
+++ Models/Login.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace SenaiAlimentos.Models;
+
+public class Login{
+
+    public int Id {get;set;}
+
+    public string? Nome {get;set;}
+
+    public string? Email {get;set;}
+
+    public string? Senha {get;set;}
+        public Login(){
+
+         }
+
+    public Login (string? nome , string? email , string? senha){
+            Nome = nome ;
+            Email = email;
+            Senha = senha;
+
+    }
+       
+
+
+}
